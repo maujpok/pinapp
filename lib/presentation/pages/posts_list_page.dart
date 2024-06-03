845f84e45276392b5cbab717b9648aa470cd9760
MauricioPortillo <maujpok@gmail.com>
2024-06-03 18:54:53 -0300

    refresh in posts list page

--- lib/presentation/pages/posts_list_page.dart
+++ lib/presentation/pages/posts_list_page.dart
@@ -10,38 +10,41 @@ class PostListPage extends StatelessWidget {
   Widget build(BuildContext context) {
     context.read<PostsBloc>().add(GetPostsEvent());
 
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Posts List Page'),
-        centerTitle: true,
-      ),
-      body: BlocBuilder<PostsBloc, PostsState>(
-        builder: (context, state) {
-          if (state.loading) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (state.errorMessage != null) {
-            return Center(
-              child: Text(state.errorMessage!),
-            );
-          } else if (state.posts != null) {
-            return SingleChildScrollView(
-              padding: EdgeInsets.symmetric(vertical: 16),
-              physics: AlwaysScrollableScrollPhysics(),
-              child: ListView.separated(
-                separatorBuilder: (context, index) => const SizedBox(height: 16),
-                itemCount: state.posts!.length,
-                shrinkWrap: true,
-                physics: NeverScrollableScrollPhysics(),
-                itemBuilder: (context, index) {
-                  return PostItemWidget(
-                    post: state.posts![index],
-                  );
-                },
-              ),
-            );
-          }
-          return Container();
-        },
+    return RefreshIndicator(
+      onRefresh: () async => context.read<PostsBloc>().add(GetPostsEvent()),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Posts List Page'),
+          centerTitle: true,
+        ),
+        body: BlocBuilder<PostsBloc, PostsState>(
+          builder: (context, state) {
+            if (state.loading) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (state.errorMessage != null) {
+              return Center(
+                child: Text(state.errorMessage!),
+              );
+            } else if (state.posts != null) {
+              return SingleChildScrollView(
+                padding: EdgeInsets.symmetric(vertical: 16),
+                physics: AlwaysScrollableScrollPhysics(),
+                child: ListView.separated(
+                  separatorBuilder: (context, index) => const SizedBox(height: 16),
+                  itemCount: state.posts!.length,
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  itemBuilder: (context, index) {
+                    return PostItemWidget(
+                      post: state.posts![index],
+                    );
+                  },
+                ),
+              );
+            }
+            return Container();
+          },
+        ),
       ),
     );
   }
